28643302a18b51fc13c463292f9c096b22244684
Jennine Punzone <jennine@Jennine-Punzones-MacBook-Pro.local>
2016-07-29 15:16:49 -0400

    assignment1

--- BamazonManager.js
+++ BamazonManager.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -22,43 +23,70 @@ connection.query('SELECT * FROM Products', function(err, results) {
 	console.log('Stock Left', row.StockQuantity);
 	console.log('-------------------------------')
 	});
+	// var nameArray = results.map(function(row){
+	// 	return row.ProductName;
+	// });
+	console.log(results.ProductName);
 	promptCustomer(results);
 });
 
 // ask users with prompt which product they want to buy and
 // how many units of the product they would like to buy.
 var promptCustomer = function(availableProducts){
+	var nameArray = availableProducts.map(function(row){
+		return row.ProductName;
+	});
 	inquirer.prompt([{
-		type: 'input',
+		type: 'list',
 		name: 'product',
-		message: 'Which product would you like to buy?'
+		message: 'Which product would you like to buy?',
+		choices: nameArray
 // store the customers choice and set it to false to make sure its a valid product
 	}]).then(function(val){
 		// console.log("val", val);
 		var customerChoice = false;
 		availableProducts.forEach(function (choice){
-			// console.log("last", choice.ProductName);
 			if (val.product === choice.ProductName) {
-				console.log("hi", val)
+				// console.log("hi", val)
 				customerChoice = true;
 				console.log('Great choice!');
-				checkQuantity();
+				checkQuantity(choice);
 			}	
 		})
 	});
 }
 
-var checkQuantity = function(answers){
-					inquirer.prompt([{
-						type: 'input',
-						name: 'quantity',
-						message: 'How many would you like?'	
-					}]).then(function(answers){
-						console.log(answers.quantity);
-					})
-				}
+var checkQuantity = function(productChoice){
+	inquirer.prompt([{
+		type: 'input',
+		name: 'quantity',
+		message: 'How many would you like?'	
+	}]).then(function(answers){
+		// console.log('flag',answers);
+		var qty = productChoice.StockQuantity;
+		// console.log(qty)
+		// console.log(productChoice);
+		
+		if (answers.quantity < qty) {
+			connection.query('UPDATE Products SET ? WHERE ?',[{
+				StockQuantity: qty - answers.quantity
+			}, { 
+				ProductName: productChoice.ProductName
+			}], function(err, res) {
+					if (err) {
+						throw err;
+					}
+					// console.log(res);
+				console.log('Congrats! You just bought ' + answers.quantity + ' ' +productChoice.ProductName + ' for ' + answers.quantity  * productChoice.Price + ' dollars!');
+				return;
+			}) 
+		} else {
+			console.log("We do not have that amount in stock to fill your order. Please choose a qunatity less than " + qty +"!")
+		}
+	})
+}
 
-
+connection.end
 
 
 
